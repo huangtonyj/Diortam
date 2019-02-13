fc81959c24136896a31ef785c13281169a0b4556
tonyjhuang <huangtonyj91@gmail.com>
2019-02-12 21:22:54 -0800

    Convert to RCC to get height and image upload image load

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,16 +1,46 @@
-import React from 'react'
+// import React from 'react'
 
-export default function EventCard({event}) {
-  const {imageSource, predictions, timestamp, videoStream} = event;
+// export default function EventCard({event}) {
+//   const {imageSource, predictions, timestamp, videoStream} = event;
 
-  console.log(predictions);
-  return (
-      <div className="event-card">
-        <img src={imageSource} alt={`${videoStream} ${timestamp}`}/>
-        <div className="prediction-box"></div>
-      </div>
-    )
-  }
+//   console.log(predictions);
+//   return (
+  //     <div className="event-card">
+  //       <img src={imageSource} alt={`${videoStream} ${timestamp}`}/>
+  //       <div className="prediction-box"></div>
+  //     </div>
+  //   )
+  // }
   
   
-
+  import React, { Component } from 'react'
+  
+  export default class EventCard extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {dimensions: {}};
+    }
+    
+    onImgLoad = ({target: img}) => {
+      // console.log(img.offsetHeight);
+      // console.log(img.offsetWidth);
+      this.setState({
+        h: img.offsetHeight, 
+        w: img.offsetWidth
+      })
+    }
+    
+    render() {
+      const {imageSource, predictions, timestamp, videoStream} = this.props.event;
+      return (
+        <div>
+          <img 
+            src={imageSource} 
+            alt={`${videoStream} ${timestamp}`}
+            onLoad={this.onImgLoad}
+          />
+          <div className="prediction-box"></div>
+        </div>
+      )
+    }
+}
