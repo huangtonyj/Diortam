bee879b5c242979011b514f0875ab43d436a1181
tonyjhuang <huangtonyj91@gmail.com>
2019-02-13 11:21:21 -0800

    Draft slider component

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
 import mockAPI from '../api/mockAPI';
 import './App.css';
+import EventCard from './EventCard';
 
 class App extends Component {
 
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      minScore: 70,
+      events: []
+    }
+  }
 
   async componentDidMount() {
-    let response = await mockAPI();
-    const events = {};
-
-    response.mockResponse.events.forEach(({imageSource, predictions, timestamp, videoStream}) => {
-      if (!events[videoStream]) { events[videoStream] = {}; }
-      events[videoStream][timestamp] = {imageSource, predictions}
-      });
-    this.setState({events})
+    // const response = await mockAPI();
+    
+    // const events = {};
+    // response.mockResponse.events.forEach(({imageSource, predictions, timestamp, videoStream}) => {
+    //     if (!events[videoStream]) { events[videoStream] = {}; }
+    //     events[videoStream][timestamp] = {imageSource, predictions}
+    //     });
+    //   this.setState({events})
+      
+    const events = await mockAPI();
+    this.setState({events: events.mockResponse.events})
   };
+
+  onSliderChange = (e) => {
+    this.setState({minScore: e.target.value})
+  }
 
 
   render() {
@@ -28,15 +43,43 @@ class App extends Component {
       // text search
       // score at least search
 
-      console.log(this.state);
-
+    console.log(this.state);
+    
+    const eventCards = this.state.events.map(event => {
+      const {timestamp, videoStream} = event;
+      return (
+        <EventCard
+          key={`${videoStream} ${timestamp}`}
+          event={event}
+        />
+      )
+    })
 
     return (
-      <div className="App">
-        <div className = "ui input" >
-          <input type="text" placeholder="Search..."/>
+      <div className="App ui grid">
+        <div className="four wide column">
+          <div class="ui icon input">
+            <input type="text" placeholder="Search..."/>
+            <i class="search icon"></i>
+          </div>
+
+          <div class="slidecontainer">
+            <input 
+              type="range" 
+              min="0" 
+              max="100" 
+              value={this.state.minScore}
+              onChange={(e) => this.onSliderChange(e)}
+              class="slider"
+            />
+            {this.state.minScore}
+          </div>
+          
         </div>
 
+        <div className = "twelve wide column">
+          {eventCards}
+        </div>
 
       </div>
     );
