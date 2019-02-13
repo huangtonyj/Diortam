f65c92ff9cd94a0e6272d5bfc692f3278b636a83
tonyjhuang <huangtonyj91@gmail.com>
2019-02-13 12:43:14 -0800

    create Slider Component

---
+++ src/components/Slider.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Slider({minScore, onSliderChange}) {
+  return (
+    <div className="slidecontainer">
+      <input 
+        type="range" 
+        min="0" 
+        max="100" 
+        value={minScore}
+        onChange={(e) => onSliderChange(e)}
+        className="slider"
+      />
+      {minScore}
+    </div>
+  )
+}
