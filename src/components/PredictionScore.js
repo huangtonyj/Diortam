2f43f4e3ddd4acd2e4a5c4e29cec10b7b6709bb1
tonyjhuang <huangtonyj91@gmail.com>
2019-02-13 10:32:41 -0800

    code PredictionScore component

--- src/components/PredictionScore.js
+++ src/components/PredictionScore.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
-export default function PredictionScore() {
+export default function PredictionScore({scores, idx, color}) {
+  console.log(scores)
+
+  scores = scores.map(score => {
+    return (
+      <tr>
+        <td>{score.label}</td>
+        <td>{score.score}</td>
+      </tr>
+    )
+  })
+
   return (
-    <div>
-      
-    </div>
+    <table className="ui celled table">
+      {scores}
+    </table>
   )
 }
