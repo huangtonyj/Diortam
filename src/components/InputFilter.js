34100910aaeb02301c7d2b85a88b9d689a681bc1
tonyjhuang <huangtonyj91@gmail.com>
2019-02-13 17:28:33 -0800

    Clean up and pass list of predictionLabels for filterOptions

--- src/components/InputFilter.js
+++ src/components/InputFilter.js
@@ -2,23 +2,7 @@ import React from 'react'
 import { Dropdown, Input } from 'semantic-ui-react'
 
 export default function InputFilter({filterOptions, searchTerm, onInputFilterChange}) {
-  const tagOptions = [
-    {
-      text: 'Important',
-      value: 'Important',
-      label: { color: 'red', empty: true, circular: true },
-    },
-  ]
   return (
-    // <div className="ui icon input">
-    //   <input 
-    //     type="text" 
-    //     placeholder="Search..."
-    //     value={searchTerm}
-    //     onChange={(e) => onSearchInputChange(e)}/>
-    //   <i className="search icon"></i>
-    // </div>
-
     <Dropdown 
       text= {searchTerm ? searchTerm : 'Filter Terms'}
       icon='filter' 
@@ -26,13 +10,6 @@ export default function InputFilter({filterOptions, searchTerm, onInputFilterCha
       className='icon'
     >
     <Dropdown.Menu>
-      <Input 
-        icon='search' 
-        iconPosition='left' 
-        className='search' 
-      />
-      <Dropdown.Divider />
-      <Dropdown.Header icon='tags' content='Tag Label' />
       <Dropdown.Menu scrolling>
         {filterOptions.map(option => 
           <Dropdown.Item 
