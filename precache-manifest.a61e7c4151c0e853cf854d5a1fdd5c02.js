58758b602fd62b5c987a743d3f55a78f81d8f75a
tonyjhuang <huangtonyj91@gmail.com>
2019-02-15 08:11:30 -0800

    Updates

---
+++ precache-manifest.a61e7c4151c0e853cf854d5a1fdd5c02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d9ad625511544157810",
+    "url": "/Diortam/static/css/main.bc98c243.chunk.css"
+  },
+  {
+    "revision": "6d9ad625511544157810",
+    "url": "/Diortam/static/js/main.6d9ad625.chunk.js"
+  },
+  {
+    "revision": "120de8d0c04162c3ff44",
+    "url": "/Diortam/static/js/runtime~main.120de8d0.js"
+  },
+  {
+    "revision": "d2e81ddb8e90c166eb23",
+    "url": "/Diortam/static/js/2.d2e81ddb.chunk.js"
+  },
+  {
+    "revision": "bb9257f5ff9ae1f9a986e1de417e074d",
+    "url": "/Diortam/index.html"
+  }
+];
