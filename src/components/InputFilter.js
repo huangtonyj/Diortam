e7e083a8c8a62d99f357bebba2b61a370856f625
tonyjhuang <huangtonyj91@gmail.com>
2019-02-14 22:34:44 -0800

    Remove "Input" component import

--- src/components/InputFilter.js
+++ src/components/InputFilter.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Dropdown, Input } from 'semantic-ui-react'
+import { Dropdown } from 'semantic-ui-react'
 
 export default function InputFilter({filterOptions, searchTerm, onInputFilterChange}) {
   return (
